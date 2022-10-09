4d9cd61b9cda2993b2ba80a06953757ac34f5d4e
dlevak92 <dino-9@gmail.com>
2022-10-09 22:32:32 +0200

    Zadatak 2 rijesen

--- Zadatak2.js
+++ Zadatak2.js
@@ -1,2 +1,16 @@
 //3. Napiši funkciju koja provjerava nalazi li se dani broj X unutar [100,150000]
 //Ispis : 53 -> “53 nije unutar [100,150000]”
+
+let broj = Number(prompt("Unesi broj:"));
+
+const veci = 100;
+const manji = 150000;
+
+function provjeri(){
+if (broj > manji && broj < veci) {
+  console.log(broj+"->je unutar [100,150000]");
+  } else {
+  console.log(broj+ "->nije unutar [100,150000]");
+    }
+}
+console.log(provjeri(broj,manji,veci));
