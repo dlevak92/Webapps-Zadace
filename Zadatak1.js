5178ffc0ede17e135a35bead06f9c21b1153fc42
dlevak92 <dino-9@gmail.com>
2022-10-09 21:52:59 +0200

    Zadatak 1 rijesen

--- Zadatak1.js
+++ Zadatak1.js
@@ -1,1 +1,9 @@
 //2. Napiši funkciju koja pretvara HRK u EUR
+
+let hrk = Number(prompt("Unesi kolicinu kuna:"));
+const tecaj = 7.5345;
+function konvertiraj(hrk,tecaj) {
+  return hrk * tecaj;
+}
+console.log("Konverzija unesene kolicine Kuna u Eure iznosi:");
+console.log(konvertiraj(hrk,tecaj));
